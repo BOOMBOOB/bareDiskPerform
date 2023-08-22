d616425b67b1896a304b6ef6cb630b2c3cb13e9f
zhangyaoru <zhang.yaoru@datatom.com>
2023-08-22 14:25:53 +0800

    增加main中日志

--- main/main.go
+++ main/main.go
@@ -12,16 +12,20 @@ import (
 )
 
 func main() {
+	fmt.Println("加载配置文件....")
 	config, err := module.LoadConfig("./config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("配置文件加载成功.")
 
 	// 获取db 连接
+	fmt.Println("获取 db 连接....")
 	db, err := module.NewDatabase(config)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("获取 db 连接成功.")
 
 	defer func() {
 		if err := db.Close(); err != nil {
@@ -34,29 +38,37 @@ func main() {
 	// 遍历配置文件中指定的盘符列表
 	for _, disk := range disks {
 		// 获取磁盘smart信息
+		fmt.Printf("获取磁盘 %v smart信息....", disk)
 		smartInfo, err := module.GetDiskSmartInfo(disk)
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Println("获取磁盘smart信息成功")
 
 		// 遍历配置文件中指定的测试类型
 		for _, iotype := range iotypes {
+			fmt.Printf("对磁盘 %v 进行 %v 负载测试....", disk, iotype)
 			fiooutput, workload, err := module.ExecuteFio(disk, iotype, 128)
 			if err != nil {
 				log.Fatal(err)
 			}
+			fmt.Println("负载测试完毕.")
 
 			// 解析 fio 执行结果
+			fmt.Println("分析 fio 测试结果....")
 			ioResult, err := module.ParseFIOOutput(fiooutput)
 			if err != nil {
 				log.Fatal(err)
 			}
+			fmt.Println("fio测试结果分析完毕.")
 
 			// 记录存储到数据库中
+			fmt.Println("记录测试结果到数据库中....")
 			err = db.SaveFIOResult(ioResult, workload, smartInfo)
 			if err != nil {
 				log.Fatal(err)
 			}
+			fmt.Println("记录完毕.")
 		}
 	}
 }
